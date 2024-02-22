5e2aef5e8787f03b70a926b4df9a53adc7487072
devjylee <wbs0810@gmail.com>
2024-02-22 16:03:28 +0900

    message

---
+++ practice.py
@@ -0,0 +1,11 @@
+name = "고양이"
+animal = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age>=3
+
+print("우리집 "+animal+"의 이름은 "+name+"에요.")
+hobby = "공놀이"
+print(name, "는 ",age,"살이며, ",hobby,"을 아주 좋아해요")
+print(name+"는 어른일까요? "+str(is_adult))
+# 정수형과 불리언형을 문자열과 같이 사용할 때는 문자열로 변환 필요
