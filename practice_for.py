b2ae915528bbd96c85bf2c141eb16f9bfbbd96aa
devjylee <wbs0810@gmail.com>
2024-03-08 20:19:19 +0900

    나도코딩6

---
+++ practice_for.py
@@ -0,0 +1,6 @@
+for waiting_no in range(1,6): # 1,2,3,4,5
+    print("대기번호: {0}".format(waiting_no))
+
+starbucks=["치이카와","하치와레","우사기"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
